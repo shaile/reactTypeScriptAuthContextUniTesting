37f93c2b002915f22cd68409adce6dbd03bb03ea
shaile <shaile.martin@gmail.com>
2022-12-06 22:21:19 +0530

    unit test

--- src/pages/users/User.tsx
+++ src/pages/users/User.tsx
@@ -5,7 +5,6 @@ import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import { CardThumb } from '@/components'
 import { useAuthState } from '@/context'
-import BasicTabs from '@/components/theme/BasicTabs'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
